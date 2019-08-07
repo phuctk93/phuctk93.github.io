98119a2a31c9af9a142e491551f4a6859f61beca
phuctk93 <phuctk93@gmail.com>
2019-08-07 14:57:59 +0700

    update contents

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const p1 = document.getElementById("p1"),
 p2 = document.getElementById("p2"),
-title = document.getElementById("title");
+title = document.getElementById("title"),
+in_main = document.getElementById("in-main");
 
 prepareSVG(p1);
 prepareSVG(p2);
@@ -8,6 +9,7 @@ prepareSVG(p2);
 TweenLite.to(p1, 3, {strokeDashoffset: 0});
 TweenLite.to(p2, 1, {strokeDashoffset: 0});
 TweenLite.fromTo(title, 3, {y: -50}, {y: 0, ease: Bounce.easeOut});
+TweenLite.fromTo(in_main, 3, {opacity: 0}, {opacity: 1}).delay(3);
 
 function prepareSVG(el) {
 	var l = el.getTotalLength();
